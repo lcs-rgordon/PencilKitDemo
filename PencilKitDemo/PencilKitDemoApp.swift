c84ee83905b7ef35ffa499b014eda667f5ad4fd8
Russell Gordon <rgordon@lcs.on.ca>
2024-05-30 09:34:33 -0400

    Initial Commit

---
+++ PencilKitDemo/PencilKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PencilKitDemoApp.swift
+//  PencilKitDemo
+//
+//  Created by Russell Gordon on 2024-05-30.
+//
+
+import SwiftUI
+
+@main
+struct PencilKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
